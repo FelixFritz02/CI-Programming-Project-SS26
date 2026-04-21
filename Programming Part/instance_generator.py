61a97dad575cf31f4f36e5c793bac3fd90b07521
Felix <felix.fritz02@gmx.de>
2026-04-21 12:27:42 +0200

    starting with programming project: creating instance generator setting up state and action space defining transition + reward function

---
+++ Programming Part/instance_generator.py
@@ -0,0 +1,17 @@
+# Generates an instance of DRAUSP using T_d time steps, K resources, and a list of capacities C_k for each resource.
+# The parameter lam controls the average number of requests in the Poisson distribution for generating the request vector q. 
+
+import random
+import numpy as np
+
+def instance(T_d=10, K=5, lam=1):
+
+    total_requests = []
+    for _ in range(T_d):
+        r = random.gauss(10, 3)  # normal distribution
+        len_q = random.randint(1, K)  # uniform integer
+        q = [np.random.poisson(lam) + 1 for _ in range(len_q)]
+        q += [0] * (K - len_q)
+        total_requests.append([r] + q)
+        
+    return total_requests
